730deb4086cafe8c66eeb51df67396bc30dd24f8
Seiya Sasaki <seiya.sasaki@laboro.ai>
2024-07-15 22:57:18 +0900

    issue #4: スコア計算統一のための一時リファクタリング修正と暫定版のテスト実装

---
+++ jssp/tests/test_eval_score.py
@@ -0,0 +1,52 @@
+from jssp.eval_score import calc_time4job, prepare_gannt_data_from
+
+
+class TestCalcTime4Job:
+    def test_all_flow_jobs(self) -> None:
+        machine_num_per_type = [1, 1, 1]
+        proc_time = [[1, 2, 3], [3, 2, 1], [4, 1, 1]]
+        machine_type_seq = [[1, 2, 3], [3, 2, 1], [2, 1, 3]]
+        num_job = 3
+        num_machine_types = 3
+        target_chromosome = [0, 0, 0, 1, 1, 1, 2, 2, 2]
+        OperationTypeMapping = {0: "flow", 1: "flow", 2: "flow"}
+
+        result = calc_time4job(
+            machine_num_per_type,
+            proc_time,
+            machine_type_seq,
+            num_job,
+            num_machine_types,
+            target_chromosome,
+            OperationTypeMapping,
+        )
+
+        # 各マシンタイプの各マシンの稼働終了時間の例
+        # assert result == {1: {0: 16}, 2: {0: 15}, 3: {0: 17}}
+
+        # 各ジョブの終了時刻
+        assert result == {0: 6, 1: 12, 2: 17}
+
+
+class TestPrepareGanntDataFrom:
+    def test_all_flow_jobs(self) -> None:
+        machine_num_per_type = [1, 1, 1]
+        proc_time = [[1, 2, 3], [3, 2, 1], [4, 1, 1]]
+        machine_type_seq = [[1, 2, 3], [3, 2, 1], [2, 1, 3]]
+        num_job = 3
+        num_machine_types = 3
+        target_chromosome = [0, 0, 0, 1, 1, 1, 2, 2, 2]
+        OperationTypeMapping = {0: "flow", 1: "flow", 2: "flow"}
+
+        result, _ = prepare_gannt_data_from(
+            machine_num_per_type,
+            proc_time,
+            machine_type_seq,
+            num_job,
+            num_machine_types,
+            target_chromosome,
+            OperationTypeMapping,
+        )
+
+        # 各ジョブの終了時刻
+        assert result == {0: 6, 1: 12, 2: 17}
--- jssp/visualize.py
+++ jssp/visualize.py
@@ -5,66 +5,6 @@
 
 # import plotly.figure_factory as ff
 
-from eval_score import make_job_record
-from genetic_algorithm import Chromosome
-
-
-def prepare_gannt_data_from(
-    machine_num_per_type: list[int],  # 0~9
-    proc_time: list[list[int]],
-    machine_type_seq: list[list[int]],
-    m_keys: list[int],  # 0~9
-    j_keys: list[int],  # 0~9
-    sequence_best: Chromosome,
-) -> tuple[dict, dict]:
-    # カウンター類の初期化
-    done_count_in_job = {key: 0 for key in j_keys}
-    time4job = {key: 0 for key in j_keys}
-    time4machine: dict[int, dict[int, int]] = {}
-    for machine_type_id in m_keys:
-        time4machine[machine_type_id + 1] = {}
-        for machine_id in range(0, machine_num_per_type[machine_type_id]):
-            time4machine[machine_type_id + 1][machine_id] = 0
-    # free_times: list[dict[str, int]] = [
-    #     # 例
-    #     # {
-    #     #     "start_time": 4, "end_time": 12, "interval": 8,
-    #     #     "machine_type_id": 2, "machine_id": 0
-    #     # }
-    # ]
-
-    j_record: dict = {}
-    for gene in sequence_best:
-        # 次に処理すべきタスクのID
-        task_no: int = done_count_in_job[gene]
-
-        # 処理時間
-        gen_t: int = int(proc_time[gene][task_no])
-
-        # ジョブ内タスクを担当する機械タイプのID
-        # gen_m: int = int(machine_type_seq[gene][task_no])
-        m_type_id: int = int(machine_type_seq[gene][task_no])
-        # タスクを担当する機械IDをここで決める (今まで処理時間が最も短い機械)
-        machine_id: int = min(time4machine[m_type_id], key=time4machine[m_type_id].get)
-
-        time4job[gene] = time4job[gene] + gen_t
-        time4machine[m_type_id][machine_id] = (
-            time4machine[m_type_id][machine_id] + gen_t
-        )
-
-        if time4machine[m_type_id][machine_id] < time4job[gene]:
-            time4machine[m_type_id][machine_id] = time4job[gene]
-        elif time4machine[m_type_id][machine_id] > time4job[gene]:
-            time4job[gene] = time4machine[m_type_id][machine_id]
-
-        # 記録を残す
-        j_record = j_record | make_job_record(
-            time4job, gene, gen_t, m_type_id, machine_id, j_record
-        )
-        done_count_in_job[gene] = done_count_in_job[gene] + 1
-
-    return time4machine, j_record
-
 
 def format_gannt_data(
     machine_num_per_type: list[int],  # 0~9
