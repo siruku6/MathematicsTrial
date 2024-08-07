38797bba4adf9793632a2ace33c41edf73285f08
Seiya Sasaki <seiya.sasaki@laboro.ai>
2024-08-07 21:02:41 +0900

    issue #6: solved la40 with solver PULP_CBC_CMD

--- jssp/la40/loader.py
+++ jssp/la40/loader.py
@@ -1,4 +1,6 @@
-def read_benchmark_task_data(benchmark_data_path: str) -> tuple[list[str], list[str]]:
+def read_benchmark_task_data(
+    benchmark_data_path: str,
+) -> tuple[list[list[int]], list[list[str]]]:
     with open(benchmark_data_path, "r") as f:
         la40_data: list = [line.rstrip("\n").split("\t") for line in f.readlines()]
 
@@ -7,8 +9,20 @@ def read_benchmark_task_data(benchmark_data_path: str) -> tuple[list[str], list[
     num_jobs, num_machines = la40_data[0]
     # print(la40_data[0], num_machines)
 
-    processing_time_per_job: list[str] = la40_data[1 : 1 + int(num_jobs)]
-    machine_sequence_per_job: list[str] = la40_data[
+    raw_processing_time_per_job: list[list[str]] = la40_data[1 : 1 + int(num_jobs)]
+    # processing_time_per_job (2次元配列) の値を int に変換
+    processing_time_per_job: list[list[int]] = [
+        [int(processing_time) for processing_time in processing_times]
+        for processing_times in raw_processing_time_per_job
+    ]
+
+    machine_sequence_per_job: list[list[str]] = la40_data[
         1 + int(num_jobs) : 1 + int(num_jobs) * 2
     ]
+    # machine_sequence_per_job (2次元配列) の値を int に変換
+    machine_sequence_per_job: list[list[int]] = [
+        [int(machine_sequence) for machine_sequence in machine_sequences]
+        for machine_sequences in machine_sequence_per_job
+    ]
+
     return processing_time_per_job, machine_sequence_per_job
